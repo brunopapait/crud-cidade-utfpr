c234c6b2e47020facef566b88ec13c56fdf111aa
brunopapait <brunohpapait@gmail.com>
2022-02-07 22:44:36 -0300

    feat: create methods of configurations to show messages and valid actions

--- src/main/java/br/com/papait/bruno/crudcidadeutfpr/CrudCidadeUtfprApplication.java
+++ src/main/java/br/com/papait/bruno/crudcidadeutfpr/CrudCidadeUtfprApplication.java
@@ -2,12 +2,33 @@ package br.com.papait.bruno.crudcidadeutfpr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class CrudCidadeUtfprApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CrudCidadeUtfprApplication.class, args);
+  }
+
+  @Bean
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+    reloadableResourceBundleMessageSource.setBasename("classpath:messages");
+    reloadableResourceBundleMessageSource.setDefaultEncoding("UTF-8");
+
+    return reloadableResourceBundleMessageSource;
+  }
+
+  @Bean
+  public Validator getValidator() {
+    LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+    localValidatorFactoryBean.setValidationMessageSource(messageSource());
+    return localValidatorFactoryBean;
   }
 
 }
--- src/main/java/br/com/papait/bruno/crudcidadeutfpr/view/Cidade.java
+++ src/main/java/br/com/papait/bruno/crudcidadeutfpr/view/Cidade.java
@@ -1,7 +1,16 @@
 package br.com.papait.bruno.crudcidadeutfpr.view;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public final class Cidade {
+
+  @NotBlank(message = "{app.cidade.blank}")
+  @Size(min = 5, max = 60, message = "{app.cidade.size}")
   private final String nomeCidade;
+
+  @NotBlank(message = "{app.estado.blank}")
+  @Size(min = 2, max = 2, message = "{app.estado.size}")
   private final String nomeEstado;
 
 
