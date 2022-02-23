a7373475855cdeec99298bdfc4dfc464b9d6b9d8
brunopapait <brunohpapait@gmail.com>
2022-02-22 23:32:28 -0300

    feat: add script sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `usuario` VALUES (1, 'brunopapait', '$2a$10$A1.sEY9LE4c2D8xMUL8heOL4sWvaGHqH5EY..MM/k.FeVkKvrwjWu');
+INSERT INTO `usuario` VALUES (2, 'brunop', '$2a$10$A1.sEY9LE4c2D8xMUL8heOL4sWvaGHqH5EY..MM/k.FeVkKvrwjWu');
+
+INSERT INTO `usuario_papeis` VALUES (1, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'admin');
