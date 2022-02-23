59e5051660a58e505efc4260bc1373b00803327e
brunopapait <brunohpapait@gmail.com>
2022-02-22 23:33:21 -0300

    feat: remove method where performed authentication in memory

--- src/main/java/br/com/papait/bruno/crudcidadeutfpr/SecurityConfig.java
+++ src/main/java/br/com/papait/bruno/crudcidadeutfpr/SecurityConfig.java
@@ -1,7 +1,9 @@
 package br.com.papait.bruno.crudcidadeutfpr;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,18 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-            .inMemoryAuthentication()
-            .withUser("brunopapait")
-            .password(passwordEncoder().encode("araruna123"))
-            .authorities("listar")
-            .and()
-            .withUser("brunop").password(passwordEncoder().encode("araruna456"))
-            .authorities("admin");
-  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
